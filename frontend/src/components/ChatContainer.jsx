66a149ce901faed743542b2cd43a69703c59e674
Sujal-g1 <sujal.work2005@gmail.com>
2026-04-03 18:15:29 +0530

    completed the major frontend part of the site , refining will be done later

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMsgTime } from '../lib/utils';
 
 const ChatContainer = ({selectedUser , setSelectedUser}) => {
+
+    const scrollEnd = useRef();
+    useEffect(()=>{
+        if(scrollEnd.current){
+            scrollEnd.current.scrollIntoView({ behavior : "smooth"})
+        }
+    },[])
+
   return  selectedUser ?(
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
             {/* header`` */}
@@ -47,11 +56,33 @@ const ChatContainer = ({selectedUser , setSelectedUser}) => {
         assets.avatar_icon : assets.profile_martin}
         className='w-7 rounded-full'
          alt="" />
-         <p className='text-gray-500'>{msg.createdAt}</p>
+         <p className='text-gray-500'>{formatMsgTime(msg.createdAt)}</p>
         </div>
         </div>
     ))}
+
+    <div ref={scrollEnd}></div>
+
+
     </div>
+
+        {/* // bottom area  */}
+        <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+        <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+        <input type="text" 
+        placeholder='message'
+        className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+        <input type="file" id='image' accept='image/png , image/jpeg' hidden />
+        <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="" 
+            className='w-5 mr-2 cursor-pointer'/>
+        </label>
+        </div>
+
+        <img src={assets.send_button} alt="" 
+        className='w-7 cursor-pointer'/>
+        </div>
+
 
 
     </div>
