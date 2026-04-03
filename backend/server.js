3c411cfe9f719c6418743555b3b2dc43bb42251c
Sujal-g1 <sujal.work2005@gmail.com>
2026-04-03 19:11:26 +0530

    Started working on backend , connected to db

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+
+import express from "express"
+import "dotenv/config";
+import cors from "cors"; 
+import http from "http"
+import { connectDB } from "./config/db.js";
+
+const app = express();
+const server = http.createServer(app)
+
+
+//middlewares
+app.use(express.json({limit:"4mb" }));
+app.use(cors());
+
+app.use("/api/status" ,(req , res)=>{
+    res.send("Server is running")
+})
+
+// connecting to db
+await connectDB();
+
+const PORT = process.env.PORT || 5002;
+server.listen(PORT ,()=>console.log("Server is running on PORT:" + PORT))
